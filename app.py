92b83936f09072f501e241681ceca6926283f1ce
Karthik <karthikk1162@gmail.com>
2026-06-24 10:50:55 +0530

    Implemented automatic database creation and imported redirect and url for and Implemented history page inside the UI

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 from prompt_optimizer import optimize_prompt
 from prompt_scorer import analyze_prompt
+from db import (
+    create_database,
+    save_prompt,
+    get_prompt_history,
+    delete_history
+)
 
 app = Flask(__name__)
+create_database()
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/history")
+def history():
+
+    history_data = get_prompt_history()
+
+    return render_template(
+
+        "history.html",
+
+        history=history_data
+
+    )
+
+@app.route("/delete-history", methods=["POST"])
+def clear_history():
+
+    delete_history()
+
+    return redirect(url_for("history"))
 
 
 @app.route("/optimize", methods=["POST"])
@@ -31,23 +58,56 @@ def optimize():
                 }
             ), 400
 
-        # Analyze the original prompt
-        analysis = analyze_prompt(user_prompt)
+        # Analyze original prompt
+        original_analysis = analyze_prompt(user_prompt)
 
-        # Optimize the prompt using Gemini
+        # Optimize prompt
         optimized_prompt = optimize_prompt(
             user_prompt,
             platform,
             prompt_type
         )
 
+        # Analyze optimized prompt
+        optimized_analysis = analyze_prompt(optimized_prompt)
+
+        improvement = (
+            optimized_analysis["score"]
+            - original_analysis["score"]
+        )
+        save_prompt(
+
+            original_prompt=user_prompt,
+
+            optimized_prompt=optimized_prompt,
+
+            platform=platform,
+
+            prompt_type=prompt_type,
+
+            original_score=original_analysis["score"],
+
+            optimized_score=optimized_analysis["score"],
+
+            score_improvement=improvement
+
+        )
+
         return jsonify(
             {
                 "success": True,
+
                 "optimized_prompt": optimized_prompt,
-                "score": analysis["score"],
-                "level": analysis["level"],
-                "improvements": analysis["improvements"]
+
+                "original_score": original_analysis["score"],
+                "original_level": original_analysis["level"],
+
+                "optimized_score": optimized_analysis["score"],
+                "optimized_level": optimized_analysis["level"],
+
+                "score_improvement": improvement,
+
+                "improvements": original_analysis["improvements"]
             }
         )
 
